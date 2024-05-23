bb22d70e03acb193446886a7152f51dce33f4221
Praveen3016 <143151914+Praveen3016@users.noreply.github.com>
2024-05-23 08:50:32 +0530

    socket

---
+++ client/src/App.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useMemo, useState } from "react"
+import {io} from "socket.io-client"
+// import {Container, TextField, Typography ,Button} from "@mui/material"
+
+function App() {
+
+  const [message , setMessage] = useState("");
+  const [room , setRoom] = useState("");
+  const [SocketId , setSocketId] = useState("");
+  const [messages , setMassages] = useState([]);
+  const [roomName , setRoomName] = useState("");
+
+
+
+
+
+  const socket = useMemo(()=> io("http://localhost:3000"),[]);
+
+  useEffect(()=>{
+    socket.on("connect" , () =>{
+      console.log("socket conneted" , socket.id )
+      setSocketId(socket.id)
+    })
+
+    socket.on("wellcome" , (message) =>{
+      // console.log(message)
+    })
+
+    socket.on("receive-message" ,(data)=>{
+      console.log(data)
+      setMassages((prew) => [...prew , data])
+    })
+  } , [])
+
+  function handleSubmit(e)
+  {
+e.preventDefault();
+socket.emit("message" ,{message , room});
+setMessage("")
+  }
+
+  function handleRoom(e){
+    e.preventDefault();
+    socket.emit("room-name" , roomName);
+    setRoomName("")
+  }
+
+  return (
+   <div>
+    <h3>{SocketId}</h3>
+    <form onSubmit={handleRoom}>
+      <h5>Join Room</h5>
+      <input onChange={(e) => setRoomName(e.target.value)} value={roomName} type="text"  /><br />
+      <button type="submit">join</button>
+    </form>
+
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="">message</label>
+      <input onChange={(e) => setMessage(e.target.value)} value={message} type="text"  /><br />
+      <label htmlFor="">socket Id</label>
+      <input onChange={(e) => setRoom(e.target.value)} value={room} type="text"  /><br />
+
+      <button type="submit">send</button>
+
+      <div>
+        {
+  messages.map((elem , index)=>{
+   return <p key={index}>{elem}</p>
+  })
+        }
+      </div>
+    </form>
+   </div>
+  )
+} 
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,67 @@
+import express from "express";
+import { Server } from "socket.io";
+import http from "http";
+import cors from "cors"
+import jwt from "jsonwebtoken"
+
+const port = 3000;
+const privateKey = "dfdfcsas"
+
+const app = express();
+app.use(cors())
+const server = http.createServer(app); // Create HTTP server instance
+
+const io = new Server(server ,{
+    cors :{
+        origin:"http://localhost:5173",
+        methods : ["GET" ,"POST"] ,
+        credentials : true
+    }
+}); // Attach Socket.io to the HTTP server instance
+
+
+
+// const user = true ;
+// io.use((socket, next) => {
+//   cookieParser()(socket.request, socket.request.res, (err) => {
+//     if (err) return next(err);
+
+//     const token = socket.request.cookies.token;
+//     if (!token) return next(new Error("Authentication Error"));
+
+//     const decoded = jwt.verify(token, privateKey);
+//     next();
+//   });
+// });
+
+app.get("/", (req, res) => {
+  return res.send("hello im working");
+});
+
+app.get("/login", (req, res) => {
+  const token = jwt.sign({ Id: 'bar' }, privateKey);
+
+  return res.cookie("token" , token , { httpOnly: true , secure : true ,sameSite :true }).json({message : "login success"})
+});
+
+io.on("connection", (socket) => { 
+  console.log("socket connected");
+  console.log("id", socket.id);
+
+   socket.on("message", ({room , message})=>{
+    console.log({room , message});
+    io.to(room).emit("receive-message" , message)
+   }) 
+
+   socket.on("room-name", (roomName)=>{
+    
+   socket.join(roomName)
+   console.log(' user joined' ,roomName )
+  }) 
+
+  socket.emit("wellcome" , `wellcome praveen in socket world ${socket.id}`)
+});
+
+server.listen(port, () => {
+  console.log(`running on port ${port}`);
+});

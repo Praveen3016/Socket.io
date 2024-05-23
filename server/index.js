a6dead34f9ce3a62b5fde5f564691f2b1145aa9e
Praveen3016 <143151914+Praveen3016@users.noreply.github.com>
2024-05-23 13:02:09 +0530

    ss

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app); // Create HTTP server instance
 
 const io = new Server(server ,{
     cors :{
-        origin:"socket-io-chat24/7.vercel.app",
+        origin:"socket-io-livechat.vercel.app",
         methods : ["GET" ,"POST"] ,
         credentials : true
     }
